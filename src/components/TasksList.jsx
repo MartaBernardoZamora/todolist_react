b27682b95c275fc4eaf279ba67e45991ce6b8fee
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-28 17:35:13 +0100

    up and down  button ok status

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -6,18 +6,28 @@ function TasksList() {
   function handleDeleteButton(id){
     setTask(currentTask => currentTask.filter((_, i) => i !== id));
   }
+  function handleOrderButtons(num, i){
+    const updatedTasks = [...task];
+    
+    const newIndex=i+num;    
+    if(newIndex<0 || newIndex>=updatedTasks.length) return;
+
+    [updatedTasks[i], updatedTasks[i+num]] = [updatedTasks[i+num], updatedTasks[i]];
+    setTask(updatedTasks);
+  }
+
   const tasks = task.map((t, i) =>
       <li key={i}>
         <p>{t}</p>
         <button
           className='upButton'
-          onClick={()=>handleOrderButtons(-1)}
+          onClick={()=>handleOrderButtons(-1, i)}
         >
           ^
         </button>
         <button 
           className='downButton'
-          onClick={()=>handleOrderButtons(1)}
+          onClick={()=>handleOrderButtons(1, i)}
         >
           v
         </button>
